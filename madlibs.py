7db8c72938161674a6b1fa7410112ccd0be49fc4
Hanae 800 <hanaehmimid@gmail.com>
2019-09-18 19:45:06 -0400

    Test

---
+++ madlibs.py
@@ -0,0 +1,23 @@
+print random
+#%%
+import random
+#%%
+verbs = ["x" , "y", "z"]
+print(verbs)
+
+#%%
+verbs = ["o" , "x" , "l"]
+print(verbs)
+
+#%%
+verbs = ["mina" , "samia" , "nada"] 
+print(verbs)
+
+#%%
+verbs = ["sing" , "shower" , "pray"]
+print("samia" + random.choice(verbs)+ "after lunch")
+
+#%%
+print (“difficult”) + random.choice(nouns) + "lovely"
+
+#%%
