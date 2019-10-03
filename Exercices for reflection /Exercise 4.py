f8731c9a15a0926b82300ca8b64d26bc3e1b34d4
danielleaneal <54646998+danielleaneal@users.noreply.github.com>
2019-10-02 22:30:48 -0400

    Update for your exercise 4!

--- Exercices for reflection /Exercise 4.py
+++ Exercices for reflection /Exercise 4.py
@@ -4,4 +4,4 @@
 print("Hello World. It's Hanae, let's do this!")
 
 #%%
-print("Hello it's Danielle, here in Hanae's repository doing the collaborating thing!")
+print("Hello it's Danielle, here in Hanae's repository doing the collaborating thing! Will it work? I'm tired!")
