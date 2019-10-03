548d8764aab436e916f5b27aa475abdf83dc0591
Hanae800 <54814586+Hanae800@users.noreply.github.com>
2019-10-02 22:34:19 -0400

    Changes #1

--- Exercices for reflection /Exercise 4.py
+++ Exercices for reflection /Exercise 4.py
@@ -5,3 +5,5 @@ print("Hello World. It's Hanae, let's do this!")
 
 #%%
 print("Hello it's Danielle, here in Hanae's repository doing the collaborating thing! Will it work? I'm tired!")
+print (" Hello it's Hanae, i recieved your request and here are my changes to the message")
+
