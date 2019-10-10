63ba35dea6e9dc2848465f893beb1293830bf4d2
Hanae 800 <hanaehmimid@gmail.com>
2019-10-09 23:07:57 -0400

    Exercice 8.2

---
+++ Exercise8_2.py
@@ -0,0 +1,15 @@
+#Same Last Character
+#%%
+
+def sum_last(firststring,secondstring):
+    
+    if firststring[-1:] == secondstring[-1:] 
+    print ("True") 
+   
+    else:
+    print("False")
+
+Sum_last("Hanae" , "Hanae")
+
+
+
